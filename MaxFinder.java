610e982a282c57a240227cc08f683aeefdcff353
shubhambendkhale <shubhambendkhale90@gmail.com>
2021-07-04 18:13:19 +0530

    Added : refactored all three to one generic method

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,77 +1,31 @@
 package com.generic;
-import java.util.Scanner;
 
 public class MaxFinder {
-    static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println("Press 1 to find maximum of Integers\nPress 2 to find maximum of Float number\nPress 3 to find maximum of String");
-        System.out.print("Enter option : ");
-        int option = scanner.nextInt();
-
-        switch (option) {
-            case 1:
-                System.out.println("Enter a size of Integer array: ");
-                int size = scanner.nextInt();
-                Integer[] integersArray = new Integer[size];
-                integersArray = printInteger(integersArray, size);
-                Integer integerMax = findMax(integersArray);
-                System.out.println("Maximum integer value is :" + integerMax);
-                break;
-            case 2:
-                System.out.println("Enter a size of float array: ");
-                size = scanner.nextInt();
-                Float[] floatsArray = new Float[size];
-                floatsArray = printFloat(floatsArray, size);
-                Float floatMax = findMax(floatsArray);
-                System.out.println("Maximum float number is :" + floatMax);
-                break;
-            case 3:
-                System.out.println("Enter a size of String array: ");
-                size = scanner.nextInt();
-                String[] stringsArray = new String[size];
-                stringsArray = printString(stringsArray, size);
-                String stringMax = findMax(stringsArray);
-                System.out.println("Maximum String is :" + stringMax);
-                break;
-            default:
-                System.out.println("Invalid input.");
-        }
-    }
-
-    public static Integer[] printInteger(Integer[] integersArray, int size) {
-        for (int i = 0; i<size; i++) {
-            System.out.println("Enter array value : ");
-            integersArray[i] = scanner.nextInt();
-        }
-        return integersArray;
-    }
-
-    public static Float[] printFloat(Float[] floatsArray, int size) {
-        for (int i = 0; i<size; i++) {
-            System.out.println("Enter array value : ");
-            floatsArray[i] = scanner.nextFloat();
-        }
-        return floatsArray;
-    }
-
-    public static String[] printString(String[] stringsArray, int size) {
-        for (int i = 0; i<size; i++) {
-            System.out.println("Enter array value : ");
-            stringsArray[i] = scanner.next();
-        }
-        return stringsArray;
-    }
-
-    // Generic type method
-    // We have to extends Comparable otherwise it will give error while using compareTo() method
-    public static <E extends Comparable> E findMax(E[] integers) {
-        E max = integers[0];
-        for (E i : integers) {
-            if (i.compareTo(max) > 0) {
-                max = i;
-            }
-        }
-        return max;
-    }
-
+	public static void main(String[] args) {
+		String firstString = "Apple";
+		String secondString = "Peach";
+		String thirdString = "Banana";
+		
+		Integer firstIntNumber = 12;
+		Integer secondIntNumber = 15;
+		Integer thirdIntNumber = 20;
+		
+		Double firstDoubleNumber = 120.2;
+		Double secondDoubleNumber = 150.2;
+		Double thirdDoubleNumber = 200.3;
+		
+		getMax(firstString,secondString,thirdString);  // method for string
+		getMax(firstIntNumber,secondIntNumber,thirdIntNumber); // method for integer
+		getMax(firstDoubleNumber,secondDoubleNumber,thirdDoubleNumber);  // method for double
+	}
+	private static <T extends Comparable<T>> void getMax(T firstString, T secondString, T thirdString) {
+		T max = firstString;
+		
+		// using java compareTo method to test maximum
+		if (secondString.compareTo(max) > 0)
+			max = secondString;
+		if (thirdString.compareTo(max) > 0)
+			max = thirdString;
+		System.out.println("The Maximum is : " +max);
+	}
 }
